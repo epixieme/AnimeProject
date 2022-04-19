afabc3b4187bf3de7e8c4dfb60542bcc4f6387b7
epixieme <kirstie_hayes@hotmail.com>
2022-04-19 19:27:38 +0100

    added embedded video to cards

--- main.js
+++ main.js
@@ -31,7 +31,9 @@ function searchRequest() {
       console.error("Error:", error);
     });
 }
+
 let index = 0;
+
 function searchDetails(data) {
   let cardWrapper = document.querySelector(".cards");
   cardWrapper.innerHTML = "";
@@ -40,7 +42,8 @@ function searchDetails(data) {
     let characterCard = document.createElement("section");
 
     let characterImage = document.createElement("img");
-
+    let vidWrapper = document.createElement("iframe");
+    vidWrapper.classList.add("videoWrapper");
     characterImage.src = item.images.jpg.image_url;
     let characterTitle = document.createElement("h3");
     characterTitle.innerText = item.title;
@@ -49,61 +52,82 @@ function searchDetails(data) {
 
     let charactertitleJapanese = document.createElement("h3");
     charactertitleJapanese.innerText = item.title_japanese;
+    let icon = document.createElement('svg')
+    
+
     if (
       item.rating === "G - All Ages" ||
-      item.rating === "PG-13 - Teens 13 or older"
+      (item.rating === "PG-13 - Teens 13 or older" &&
+        item.trailer.embed_url != null)
     ) {
       characterCard.classList.add("card");
       characterCard.appendChild(characterImage);
       characterCard.appendChild(characterTitle);
-      characterCard.appendChild(translate);
+      // characterCard.appendChild(translate);
       characterCard.appendChild(charactertitleJapanese);
       charactertitleJapanese.classList.add("hide");
       cardWrapper.appendChild(characterCard);
       translate.addEventListener("click", () => {
         charactertitleJapanese.classList.remove("hide");
       });
-      }
+    }
 
-      let index=0
-     
-      let iframe = document.querySelector("iframe");
-      // let id = ''
-      // let splitUrl=''
-      characterCard.addEventListener("click", ()=>{
-        // clearInterval(animeInterval)
-        imageChanger()
-      })
+    let index = 0;
+
+    let iframe = document.querySelector("iframe");
+
+    characterCard.addEventListener("mouseover", () => {
+      imageMouseOver();
+    });
+    characterCard.addEventListener("mouseleave", () => {
+      imageMouseOut();
+    });
+
+    setInterval(() => {
+      if (data.data[index].trailer.embed_url) {
+        id = `?controls=0&autoplay=1&mute=0&playlist=${data.data[index].trailer.youtube_id}&loop=1`;
+        splitUrl = data.data[index].trailer.embed_url.split(`?`)[0].toString();
+        iframe.src = `${splitUrl} ${id}`;
+      } /// this plays videos automatically
+
+      index++;
+    }, 20000); // tackle tomorrow
+
+    function imageMouseOver() {
+      // closure
+
+      if (characterImage) {
+        characterImage.classList.add("hide");
+        vidWrapper.classList.remove("hide");
+        characterCard.appendChild(vidWrapper);
+        characterCard.insertBefore(vidWrapper, characterTitle);
+        youTubeid = `?controls=0&autoplay=1&mute=0&playlist=${item.trailer.youtube_id}&loop=1`;
+        splitUrl = item.trailer.embed_url.split(`?`)[0].toString();
+        vidWrapper.src = `${splitUrl} ${youTubeid}`;// join youtube string
        
-    setInterval(()=>{
-      
-        // iframe.src=''
-   
+      }
+    }
 
-          // let splitUrl= item.trailer.embed_url.split(`?`)[0].toString()
-          if(data.data[index].trailer.embed_url){
-          id = `?controls=0&autoplay=1&mute=0&playlist=${ data.data[index].trailer.youtube_id}&loop=1`;
-          splitUrl = data.data[index].trailer.embed_url.split(`?`)[0].toString();
-          iframe.src = `${splitUrl} ${id}`;} /// this plays videos automatically 
-        
-        index++
-      },20000) // tackle tomorrow
+    function addToFavourites(){
+      // closure
+    let favourites = document.querySelector('.favourites')
+    // if button is clicked then add to favourites
 
   
-
-      function imageChanger() {
-        // closure
-  
-         iframe.src = ``;
-       
-          // let splitUrl= item.trailer.embed_url.split(`?`)[0].toString()
-          id = `?controls=0&autoplay=1&mute=0&playlist=${item.trailer.youtube_id}&loop=1`;
-          splitUrl = item.trailer.embed_url.split(`?`)[0].toString();
-          iframe.src = `${splitUrl} ${id}`;
-        
-          
-      }
+    //   if ( video ) {
+    //     video.pause();
+    //   }
+    // };
     
+    }
+
+    function imageMouseOut() {
+      // closure
+      if (characterImage) {
+        characterImage.classList.remove("hide");
+        vidWrapper.classList.add("hide");
+      }
+    }
   });
 }
 
