f7fdd997a197979103b4c10badd0ccab457cee5f
epixieme <kirstie_hayes@hotmail.com>
2022-05-19 15:24:11 +0100

    slider enhancement

--- main.js
+++ main.js
@@ -1,35 +1,32 @@
 let button = document.querySelector("button");
 button.addEventListener("click", searchRequest);
-
-function mainPageFetch() {
-  fetch(`https://api.jikan.moe/v4/top/anime`)
-    .then((response) => response.json())
-    .then((data) => {
-      searchDetails(data);
-      // imageChanger(data)
-    })
-    .catch((error) => {
+async function mainPageFetch() {
+  const response = await fetch(`https://api.jikan.moe/v4/top/anime`)
+  try{const data = await response.json()
+    searchDetails(data);
+  }
+    catch(error) {    
       console.error("Error:", error);
-    });
+    }
 }
 
 mainPageFetch();
 
-function searchRequest() {
+async function searchRequest() {
   let choice = document.querySelector("input").value;
-  fetch(`https://api.jikan.moe/v4/anime?q=${choice}`)
-    .then((response) => response.json())
-    .then((data) => {
+  let response = await fetch(`https://api.jikan.moe/v4/anime?q=${choice}`)
+  try{ const data = await response.json()
+    
       if (choice === "") {
         return;
       } else {
         searchDetails(data);
         // imageChanger(data)
       }
-    })
-    .catch((error) => {
+    }   
+    catch(error)  {
       console.error("Error:", error);
-    });
+    };
 }
 
 let index = 0;
@@ -63,7 +60,7 @@ function searchDetails(data) {
     if (
       item.rating === "G - All Ages" ||
       (item.rating === "PG-13 - Teens 13 or older" &&
-        item.trailer.embed_url != null)
+        item.trailer.embed_url != null)// not empty
     ) {
       cardWrapper.appendChild(characterCard);
       characterCard.classList.add("card");
@@ -74,11 +71,9 @@ function searchDetails(data) {
       characterCard.appendChild(characterTitle);
 
       // charactertitleJapanese.classList.add("hide");
-    }
+  
 
-    let index = 0;
-
-    let iframe = document.querySelector("iframe");
+ 
 
     characterCard.addEventListener("mouseover", () => {
       imageMouseOver();
@@ -87,27 +82,8 @@ function searchDetails(data) {
       imageMouseOut();
     });
 
-    setInterval(() => {
-      if (data.data[index].trailer.embed_url) {
-      
-        id = `?controls=0&autoplay=1&mute=0&playlist=${data.data[index].trailer.youtube_id}&loop=1`;
-        splitUrl = data.data[index].trailer.embed_url.split(`?`)[0].toString();
-        let movie =`${splitUrl} ${id}`
-        iframe.src = movie
-     
-      }
-      index++;
-      console.log(item)
-       /// this plays videos automatically
-      // for (i = 0; i < data.data.length; i++) {
-      //   iframe.src =''
-      // }
-     
-      if (index  > data.data.length) {index = 1}
-   
-    
-    }, 10000); // tackle tomorrow
-
+  
+  }
     function imageMouseOver() {
       // closure
 
@@ -126,6 +102,29 @@ function searchDetails(data) {
         plusIcon.addEventListener("click", addToFavourites);
       }
     }
+  
+  let index = 0;
+
+  let iframe = document.querySelector("iframe");
+
+  setInterval(() => {
+    if (data.data[index].trailer.embed_url) {
+    
+      id = `?controls=0&autoplay=1&mute=0&playlist=${data.data[index].trailer.youtube_id}&loop=1`;
+      splitUrl = data.data[index].trailer.embed_url.split(`?`)[0].toString();
+      let movie =`${splitUrl} ${id}`
+      iframe.src = movie
+   
+    }
+    
+    index++;
+  
+    if (index  > data.data.length) {index = 1}
+ 
+  
+  }, 10000);
+
+
     let favouritesLink = document.querySelector(".favourites");
     favouritesLink.addEventListener("click", getFavouritesPage);
     // favouritesLink.addEventListener("click", removeFavourites);
@@ -137,29 +136,33 @@ function searchDetails(data) {
         main.remove();
       }
 
-      let array = localStorage.getItem("favourites").split(",");
+      let array = localStorage.getItem("favourites").split(",");// creates an array from the inner card details using the comma
 
       array.forEach((item) => {
         console.log(item);
         let card = document.createElement("card");
         card.classList.add("card");
         card.innerHTML = item;
-        cardWrapper.appendChild(card);
+        cardWrapper.appendChild(card); // create cards in favourites and append the details from the array
       });
     }
 
     function addToFavourites(event) {
-      if (localStorage.getItem("favourites") === null) {
+    
+      if (localStorage.getItem("favourites") === null) { // if local storage is null then == if this condition did not exist we would get an error when then trying to add more cards
         localStorage.setItem(
           "favourites",
-          event.target.parentNode.parentNode.innerHTML
+          event.target.parentNode.parentNode.innerHTML// this sets the item to the target card because event.target is set to the favourites button which we use to click to need to get the parent node which is the card
+         
         );
+       
       } else {
         let cardDetails =
           localStorage.getItem("favourites") +
           "," +
-          event.target.parentNode.parentNode.innerHTML;
-        localStorage.setItem("favourites", cardDetails);
+          event.target.parentNode.parentNode.innerHTML;// this gets the card as a string and then concatenates another every time we click - comma seperated to seperate eachcard detail so later we can split into an array
+        localStorage.setItem("favourites", cardDetails);// sets the carddetails to local storage
+        console.log('card' + cardDetails)
       }
     }
 
@@ -187,4 +190,4 @@ function searchDetails(data) {
 /// on click of image play video in background and then get some stats on the characters.
 //translate name to japanese on click
 //horizontal scroll to keep things in frame
-
+// refactor everything to a class
