8be3b57e39d181e29dccec3c17244b922fbcd118
Kirstie <39728053+epixieme@users.noreply.github.com>
2024-06-14 17:42:44 +0100

    add card to favourites feature

--- main.js
+++ main.js
@@ -1,32 +1,35 @@
 let button = document.querySelector("button");
 button.addEventListener("click", searchRequest);
 async function mainPageFetch() {
-  const response = await fetch(`https://api.jikan.moe/v4/top/anime`)
-  try{const data = await response.json()
+  const response = await fetch(`https://api.jikan.moe/v4/top/anime`);
+  try {
+    const data = await response.json();
+    console.log(data);
     searchDetails(data);
+  } catch (error) {
+    console.error("Error:", error);
   }
-    catch(error) {    
-      console.error("Error:", error);
-    }
 }
 
 mainPageFetch();
 
 async function searchRequest() {
   let choice = document.querySelector("input").value;
-  let response = await fetch(`https://api.jikan.moe/v4/anime?q=${choice}`)
-  try{ const data = await response.json()
-    
-      if (choice === "") {
-        return;
-      } else {
-        searchDetails(data);
-        // imageChanger(data)
-      }
-    }   
-    catch(error)  {
-      console.error("Error:", error);
-    };
+  let response = await fetch(`https://api.jikan.moe/v4/anime?q=${choice}`);
+  try {
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    if (choice === "") {
+      return;
+    } else {
+      searchDetails(data);
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
 
 let index = 0;
@@ -60,7 +63,7 @@ function searchDetails(data) {
     if (
       item.rating === "G - All Ages" ||
       (item.rating === "PG-13 - Teens 13 or older" &&
-        item.trailer.embed_url != null)// not empty
+        item.trailer.embed_url != null) // not empty
     ) {
       cardWrapper.appendChild(characterCard);
       characterCard.classList.add("card");
@@ -71,19 +74,14 @@ function searchDetails(data) {
       characterCard.appendChild(characterTitle);
 
       // charactertitleJapanese.classList.add("hide");
-  
 
- 
-
-    characterCard.addEventListener("mouseover", () => {
-      imageMouseOver();
-    });
-    characterCard.addEventListener("mouseleave", () => {
-      imageMouseOut();
-    });
-
-  
-  }
+      characterCard.addEventListener("mouseover", () => {
+        imageMouseOver();
+      });
+      characterCard.addEventListener("mouseleave", () => {
+        imageMouseOut();
+      });
+    }
     function imageMouseOver() {
       // closure
 
@@ -102,32 +100,30 @@ function searchDetails(data) {
         plusIcon.addEventListener("click", addToFavourites);
       }
     }
-  
-  let index = 0;
 
-  let iframe = document.querySelector("iframe");
-
-  setInterval(() => {
-    if (data.data[index].trailer.embed_url) {
-    
-      id = `?controls=0&autoplay=1&mute=0&playlist=${data.data[index].trailer.youtube_id}&loop=1`;
-      splitUrl = data.data[index].trailer.embed_url.split(`?`)[0].toString();
-      let movie =`${splitUrl} ${id}`
-      iframe.src = movie
-   
+    function setTheIntervalForTheVids() {
+      let index = 0;
+      let iframe = document.querySelector("iframe");
+      setInterval(() => {
+        if (data.data[index].trailer.embed_url) {
+          id = `?controls=0&autoplay=1&mute=0&playlist=${data.data[index].trailer.youtube_id}&loop=1`;
+          splitUrl = data.data[index].trailer.embed_url
+            .split(`?`)[0]
+            .toString();
+          let movie = `${splitUrl} ${id}`;
+          iframe.src = movie;
+        }
+        index++;
+        if (index > data.data.length) {
+          index = 1;
+        }
+      }, 10000);
     }
-    
-    index++;
-  
-    if (index  > data.data.length) {index = 1}
- 
-  
-  }, 10000);
 
+    setTheIntervalForTheVids();
 
     let favouritesLink = document.querySelector(".favourites");
     favouritesLink.addEventListener("click", getFavouritesPage);
-    // favouritesLink.addEventListener("click", removeFavourites);
 
     function getFavouritesPage() {
       let main = document.querySelector("main");
@@ -136,10 +132,9 @@ function searchDetails(data) {
         main.remove();
       }
 
-      let array = localStorage.getItem("favourites").split(",");// creates an array from the inner card details using the comma
+      let array = localStorage.getItem("favourites").split(","); // creates an array from the inner card details using the comma
 
       array.forEach((item) => {
-        console.log(item);
         let card = document.createElement("card");
         card.classList.add("card");
         card.innerHTML = item;
@@ -148,21 +143,16 @@ function searchDetails(data) {
     }
 
     function addToFavourites(event) {
-    
-      if (localStorage.getItem("favourites") === null) { // if local storage is null then == if this condition did not exist we would get an error when then trying to add more cards
+      const card = event.target.parentElement.parentElement.innerHTML;
+      if (localStorage.getItem("favourites") === null) {
+        localStorage.setItem("favourites", card);
+      } else if (localStorage.getItem("favourites").includes(card)) {
+        return;
+      } else {
         localStorage.setItem(
           "favourites",
-          event.target.parentNode.parentNode.innerHTML// this sets the item to the target card because event.target is set to the favourites button which we use to click to need to get the parent node which is the card
-         
-        );
-       
-      } else {
-        let cardDetails =
-          localStorage.getItem("favourites") +
-          "," +
-          event.target.parentNode.parentNode.innerHTML;// this gets the card as a string and then concatenates another every time we click - comma seperated to seperate eachcard detail so later we can split into an array
-        localStorage.setItem("favourites", cardDetails);// sets the carddetails to local storage
-        console.log('card' + cardDetails)
+          `${localStorage.getItem("favourites")},${card}`
+        ); // add the card to the array
       }
     }
 
